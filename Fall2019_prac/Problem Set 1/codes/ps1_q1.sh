762ca1add8cceba8f33c6cd48fa666d73f140196
Randyzhang98 <601981285@qq.com>
2019-09-22 22:23:00 -0700

    renewable

--- Fall2019_prac/Problem Set 1/codes/ps1_q1.sh
+++ Fall2019_prac/Problem Set 1/codes/ps1_q1.sh
@@ -13,7 +13,7 @@ fi
 head --line=1 $file | tr "," "\n" > recs_names.txt
 
 # c Write a one-liner that uses ‘recs_names.txt’ to find the column positions for the id and replicate weight columns in the RECS data and then re-formats these positions as a single, comma-separated string. [Hint: For the final step, use the -s and -d options to the command paste.]
-grep -rn 'DOEID\|BRRWT' recs_names.txt | tr ":" "\n" |awk 'NR%2 ==1' | tr '\n' ',' | awk '{sub(/.$/,"")}1'
+grep -rn 'DOEID\|^BRRWT' recs_names.txt | tr ":" "\n" |awk 'NR%2 ==1' | tr '\n' ',' | awk '{sub(/.$/,"")}1'
 # DESCRIPTION: firstly, I use grep to catch the index of id and replicate weight columns with the matched column names, then I transform the ':' to new line characters and place the index in the odd lines and the columns names in the even lines. Thus I can use the awk command to only output the index in the odd lines and transform the new line characters to commas. Finally I deleted the last rest comma in the string.
 
 # d Store the result from the previous one-liner in the variable ‘cols’. Use this variable to write a one-liner that extracts the id and replicate weight columns from the recs data and writes them to recs_weights.csv. [Hint: In the first step, use a construction such as cols=$(...).]
