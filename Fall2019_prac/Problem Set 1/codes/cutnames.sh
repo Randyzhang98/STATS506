4b1615fc00f4eef4127029a4165a05c7e1b2c34d
Randyzhang98 <601981285@qq.com>
2019-09-22 22:31:38 -0700

    cut

---
+++ Fall2019_prac/Problem Set 1/codes/cutnames.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+#--------------------------------------------
+# author：Sijun Zhang
+# last change date: 09/21/2019 
+#--------------------------------------------
+for arg in "$@"
+do
+    file=$arg
+done
+echo $file
