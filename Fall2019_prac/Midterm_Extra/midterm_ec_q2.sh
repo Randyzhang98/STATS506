0c5899f4d58822f9e464669e179b61aa67e6a6ad
Randyzhang98 <601981285@qq.com>
2019-10-28 18:11:24 -0400

    q2

--- Fall2019_prac/Midterm_Extra/midterm_ec_q2.sh
+++ Fall2019_prac/Midterm_Extra/midterm_ec_q2.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 #--------------------------------------------
 # author：Sijun Zhang
-# last change date: 09/21/2019 
+# last change date: 09/21/2019
 #--------------------------------------------
 
 grep -rn -E '(gather\(|spread\()' *.R*
+
+# ps1_q2.R:143:  spread(origin, prop, fill = '-') %>%
+# ps1_q3.R:28:weights_long = gather(weights, key = 'repl', value = 'w', BRRWT1:BRRWT96) %>%
+# ps1_q3.R:197:  spread(urban, est) %>%
+# ps1_q3.R:206:  spread(urban, r) %>%
+# ps1_q3.R:222:               spread(urban, ci),
+# PS1_Solutions.Rmd:277:  spread(Rurality, `Average Electricity Usage, kwh/home (95% CI)` ) %>%
